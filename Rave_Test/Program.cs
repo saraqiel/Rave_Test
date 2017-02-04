0da5bc8edd84eb8350ebf41cab811b7c6ecbd2bf
saraqiel <iw0.thedot@gmail.com>
2017-02-03 22:02:27 -0500

    removed \n from string literals

--- Rave_Test/Program.cs
+++ Rave_Test/Program.cs
@@ -11,7 +11,7 @@ namespace Rave_Test
           static void Main(string[] args)
           {
                string instructions = @"For full tutorial on getting a Visual Studio solution/project setup with GitHub" +
-                                     @"\n go here: https://www.infragistics.com/community/blogs/dhananjay_kumar/archive/2016/07/21/step-by-step-working-with-github-repository-and-visual-studio-2015.aspx \n" +
+                                     @" go here: https://www.infragistics.com/community/blogs/dhananjay_kumar/archive/2016/07/21/step-by-step-working-with-github-repository-and-visual-studio-2015.aspx" +
                                      "\n" +
                                      "1. Create a blank repository in Github ensuring that no README document is generated\n" +
                                      "2. Copy the repository URL\n" +
