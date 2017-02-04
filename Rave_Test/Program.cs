ab01340f722f7fbb34159ba1aa2ddf2192e75720
saraqiel <iw0.thedot@gmail.com>
2017-02-03 22:38:04 -0500

    expanded on the instructions

--- Rave_Test/Program.cs
+++ Rave_Test/Program.cs
@@ -11,12 +11,20 @@ namespace Rave_Test
           static void Main(string[] args)
           {
                string instructions = @"For full tutorial on getting a Visual Studio solution/project setup with GitHub" +
-                                     @" go here: https://www.infragistics.com/community/blogs/dhananjay_kumar/archive/2016/07/21/step-by-step-working-with-github-repository-and-visual-studio-2015.aspx" +
-                                     "\n" +
+                                     "go here: https://www.infragistics.com/community/blogs/dhananjay_kumar/archive/2016/07/21/step-by-step-working-with-github-repository-and-visual-studio-2015.aspx" +
+                                     "CREATING A REPOSITORY\n" +
                                      "1. Create a blank repository in Github ensuring that no README document is generated\n" +
                                      "2. Copy the repository URL\n" +
-                                     @"3. Download and Install Git from http://git-scm.com/download/win \n";
-               Console.WriteLine("Hello World");
+                                     "3. Download and Install Git from http://git-scm.com/download/win \n" +
+                                     "4. Create or Open a solution in Visual Studio \n" +
+                                     "5. Right-Click the solution and select \"Add Solution to Source Control\" \n" +
+                                     "6. Open Team Explorer by either pressing CTRL+M or navigating to \"View->Team Explorer\" \n" +
+                                     "7. Click Sync \n" +
+                                     "8. Enter the URL you copied into the \"Publish to Remote Repository\" \n" +
+                                     "9. Enter your Github credentials \n" +
+                                     "6. Verify your projects been published by navigating to GitHub and viewing your projects \n" +
+                                     "FINITO!";
+               Console.WriteLine("Hello World!");
                Console.WriteLine(instructions);
                Console.ReadLine();
           }
