91081d07e69d6b2f437bc4a523d92e2db5be6845
saraqiel <iw0.thedot@gmail.com>
2017-02-03 22:59:52 -0500

    added commit instructions

--- Rave_Test/Program.cs
+++ Rave_Test/Program.cs
@@ -10,11 +10,11 @@ namespace Rave_Test
      {
           static void Main(string[] args)
           {
-               string instructions = @"For full tutorial on getting a Visual Studio solution/project setup with GitHub" +
-                                     "go here: https://www.infragistics.com/community/blogs/dhananjay_kumar/archive/2016/07/21/step-by-step-working-with-github-repository-and-visual-studio-2015.aspx" +
-                                     "CREATING A REPOSITORY\n" +
-                                     "1. Create a blank repository in Github ensuring that no README document is generated\n" +
-                                     "2. Copy the repository URL\n" +
+               string create_instructions = "For full tutorial on getting a Visual Studio solution/project setup with GitHub \n" +
+                                     "go here: https://www.infragistics.com/community/blogs/dhananjay_kumar/archive/2016/07/21/step-by-step-working-with-github-repository-and-visual-studio-2015.aspx \n\n" +
+                                     "CREATING A REPOSITORY \n" +
+                                     "1. Create a blank repository in Github ensuring that no README document is generated \n" +
+                                     "2. Copy the repository URL \n" +
                                      "3. Download and Install Git from http://git-scm.com/download/win \n" +
                                      "4. Create or Open a solution in Visual Studio \n" +
                                      "5. Right-Click the solution and select \"Add Solution to Source Control\" \n" +
@@ -24,8 +24,16 @@ namespace Rave_Test
                                      "9. Enter your Github credentials \n" +
                                      "6. Verify your projects been published by navigating to GitHub and viewing your projects \n" +
                                      "FINITO!";
+               string committing_instructions = "COMMITTING CHANGES \n" + 
+                                                "1. After changing the code, right click on the solution and select \"Commit...\" \n" +
+                                                "2. Enter a comment describing the changes made \n" + 
+                                                "3. A notice will be generated with a commit identifier, click Sync to push changes to the repo \n" +
+                                                "4. You should get a notification letting you know if the push was successful or not \n"
+                                                ;
+
                Console.WriteLine("Hello World!");
-               Console.WriteLine(instructions);
+               Console.WriteLine(create_instructions);
+               Console.WriteLine(committing_instructions);
                Console.ReadLine();
           }
      }
